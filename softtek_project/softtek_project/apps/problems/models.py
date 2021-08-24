e43ae61600c2a941aea2abbe81c99a8e470f3787
EiderDiaz <eiderdiazmoraila@hotmail.com>
2021-08-24 00:40:33 -0500

    created detecting_change model and superuser

--- softtek_project/softtek_project/apps/problems/models.py
+++ softtek_project/softtek_project/apps/problems/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class detecting_change(models.Model):
+    date = models.DateField(blank=False,null=False)
+    was_rainy = models.BooleanField(blank=False,null=False)
